d7d467471e5cdc8c836dcf7c68b3c08921d4123a
lmc <minch819@gmail.com>
2025-04-29 20:05:53 +0900

    DB연결, Controller, Service 추가

--- src/modules/user/controllers/controller.ts
+++ src/modules/user/controllers/controller.ts
@@ -1,15 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { UserService } from '../service/service';
 
-// 유저 목록 가져오기
-export const getUsers = (req: Request, res: Response) => {
-  res.json([
-    { id: 1, name: "철수" },
-    { id: 2, name: "영희" },
-  ]);
-};
-
-// 유저 한명 가져오기
-export const getUserById = (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.json({ id, name: `사용자 ${id}` });
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await UserService.getAllUsers();
+    if (!users || users.length === 0) {
+         res.status(204).send();
+         return;
+    }
+    res.json(users);
+    return
+} catch (error) {
+    console.error('사용자 조회 중 오류 발생:', error);
+    res.status(500).json({ message: '사용자 조회에 실패했습니다.' }); 
+    return;
+}
 };
